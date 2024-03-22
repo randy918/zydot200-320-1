428e4b802d4ee03d89a4c0a2266baae5ae748c3c
Randy Miller <randy918@gmail.com>
2024-03-21 20:32:04 -0500

    Feat: Swiper works mobile and default 3-21-24-2032

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    autoplay: {
+      delay: 6500,
+    },
+    pagination: {
+      el: "#swiper-1 .swiper-pagination",
+      clickable: true,
+    },
+    lazyloading: true,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+  
+  });
+  
+  
