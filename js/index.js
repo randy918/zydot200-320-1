79b39ad7bebe22d076267ef1e970798f5c320f54
Randy Miller <randy918@gmail.com>
2024-03-23 20:57:16 -0500

    3-23-24-2057  Status Quo

--- js/index.js
+++ js/index.js
@@ -20,6 +20,30 @@ const swiper = new Swiper(".swiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+  });
+
+const swiperub = new Swiper(".swiperub", {
+    // Optional parameters
+    autoplay: {
+      delay: 6500,
+    },
+    pagination: {
+      el: "#swiper-1 .swiper-pagination",
+      clickable: true,
+    },
+    lazyloading: true,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   
   
   });
