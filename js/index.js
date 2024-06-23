9905d101441f522376b7a10dd8eb80abc6659758
Randy Miller <randy918@gmail.com>
2024-06-22 19:57:24 -0500

    achieved one pass of carousel  6-22-24-1957

--- js/index.js
+++ js/index.js
@@ -1,38 +1,27 @@
 //>  ████████████████████████████████████  GLOBAL VARIABLES
 
+
+//>  ████████████████████████████████████  QUERY SELECTORS
+
+
+//>  ████████████████████████████████████  FUNCTIONS
+
+
+
+const images = document.querySelectorAll('.carousel img');
+const dots = document.querySelector('.carouselDots');
+const desc = document.querySelector('.carouselDesc');
+
 let speed = 3000;
 let index = 0;
 let intervalID;
 
-//>  ████████████████████████████████████  QUERY SELECTORS
-
-const images = document.querySelectorAll('.carousel img');
-const dots = document.querySelector('.carousel Dots');
-const desc = document.querySelector('.carouselDesc');
-
-images.forEach((image, i) => {
-  const span = document.createElement('span');
-  span.className = 'dot';
-  if (i === 0) span.classList.add('active');
-  span.addEventListener('click', () =>  {
-
-    index = i;
-    startInterval();
-    updateContent();
-
-  });
-  dots.appendChild(span);
-
-})
-
-//>  ████████████████████████████████████  FUNCTIONS
-
 function startInterval() {
-if (intervalID) clearInterval(intervalID);
-intervalID = setInterval(() => {
+
+  setInterval(() => {
     index++;
-    if (index === images.length) index = 0;
     updateContent();
+
   }, speed);
 
 }
@@ -42,10 +31,9 @@ function updateContent() {
   images.forEach(item => item.classList.remove('active'));
   images[index].classList.add('active');
 
-  const dots = document.querySelectorAll('.dot');
-  dots.forEach(item => item.classList.remove('active'))
-  dots[index].classList.add('active');
-
 }
 
 //>  ████████████████████████████████████  ACTUAL PROGRAM
+
+startInterval()
+updateContent()
