e18a5bac6542b9a66cf66aa43197c7ef918662e1
Randy Miller <randy918@gmail.com>
2024-06-22 19:10:39 -0500

    rewriting attempt one,  more close  6-22-24-1910

--- js/index.js
+++ js/index.js
@@ -4,11 +4,32 @@ let speed = 3000;
 let index = 0;
 let intervalID;
 
+//>  ████████████████████████████████████  QUERY SELECTORS
+
+const images = document.querySelectorAll('.carousel img');
+const dots = document.querySelector('.carousel Dots');
+const desc = document.querySelector('.carouselDesc');
+
+images.forEach((image, i) => {
+  const span = document.createElement('span');
+  span.className = 'dot';
+  if (i === 0) span.classList.add('active');
+  span.addEventListener('click', () =>  {
+
+    index = i;
+    startInterval();
+    updateContent();
+
+  });
+  dots.appendChild(span);
+
+})
+
 //>  ████████████████████████████████████  FUNCTIONS
 
 function startInterval() {
-
-  setInterval(() => {
+if (intervalID) clearInterval(intervalID);
+intervalID = setInterval(() => {
     index++;
     if (index === images.length) index = 0;
     updateContent();
@@ -21,12 +42,10 @@ function updateContent() {
   images.forEach(item => item.classList.remove('active'));
   images[index].classList.add('active');
 
+  const dots = document.querySelectorAll('.dot');
+  dots.forEach(item => item.classList.remove('active'))
+  dots[index].classList.add('active');
+
 }
-
-//>  ████████████████████████████████████  QUERY SELECTORS
-
-const images = document.querySelectorAll('.carousel img');
-const dots = document.querySelector('.carousel Dots');
-const desc = document.querySelector('.carouselDesc');
 
 //>  ████████████████████████████████████  ACTUAL PROGRAM
