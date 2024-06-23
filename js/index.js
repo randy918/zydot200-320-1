0d45f64e9dc9343baf4deafe0f4251cd0128219d
Randy Miller <randy918@gmail.com>
2024-06-23 17:27:49 -0500

    have nice resiable with dots.--need to remove dots 6-23-24-1727

--- js/index.js
+++ js/index.js
@@ -4,52 +4,54 @@
 
 //>  ████████████████████████████████████  FUNCTIONS
 
-const images = document.querySelectorAll('.carousel img');
-const dots = document.querySelector('.carouselDots');
-const desc = document.querySelector('.carouselDesc');
-console.log(images);
-console.log(dots);
+const images = document.querySelectorAll('#carousel img');
+const dots = document.querySelector('#dots');
+const desc = document.querySelector('#desc');
 
-let speed = 7000;
+let speed = 7500;
 let index = 0;
 let intervalID;
-startInterval()
 
 images.forEach((image, i) => {
-  const span = document.createElement('span');
-  span.className = 'dot';
-  if (i === 0) span.classList.add('active');
-  span.addEventListener('click', () => {
-    index = i;
-updateContent()
-  });
-  dots.appendChild(span);
+
+    const span = document.createElement('span');
+    span.className = 'dot';
+    if (i === 0) span.classList.add('active');
+
+    span.addEventListener('click', () => {
+        index = 1;
+        startInterval();
+        updateContent();
+
+    });
+    dots.appendChild(span);
 
 })
 
+startInterval();
+
 function startInterval() {
-if (intervalID) clearInterval(intervalID);
 
-  intervalID = setInterval(() => {
-    index++;
-    if (index === images.length) index = 0;
+    if (intervalID) clearInterval(intervalID);
 
-    updateContent();
-
-  }, speed);
+    intervalID = setInterval(() => {
+        index++;
+        if (index === images.length) index = 0;
+        updateContent();
+    }, speed);
 
 }
-updateContent()
+
+updateContent();
+
 function updateContent() {
 
-  images.forEach(item => item.classList.remove('active'));
-  images[index].classList.add('active');
+    images.forEach(item => item.classList.remove('active'));
+    images[index].classList.add('active');
 
-  // const dots = document.querySelectorAll('.carouselDots');
-  dots.forEach(item => item.classList.remove('active'));
-  // dots[index].classList.add('active');
-  
-
+    const dots = document.querySelectorAll('.dot');
+    dots.forEach(item => item.classList.remove('active'));
+    dots[index].classList.add('active');
 
 }
 
