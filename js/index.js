ba5a5dada70e5011aeb03753c790d38254d0738f
Randy Miller <randy918@gmail.com>
2024-06-22 20:32:52 -0500

    achieved tow of dots

--- js/index.js
+++ js/index.js
@@ -12,14 +12,31 @@ const images = document.querySelectorAll('.carousel img');
 const dots = document.querySelector('.carouselDots');
 const desc = document.querySelector('.carouselDesc');
 
-let speed = 3000;
+let speed = 7000;
 let index = 0;
 let intervalID;
+
+images.forEach((image, i) => {
+  const span = document.createElement('span');
+  span.className = 'dot';
+  if (i === 0) span.classList.add('active');
+  span.addEventListener('click', () => {
+    index = i;
+    startInterval()
+updateContent()
+  });
+  dots.appendChild(span);
+
+
+
+})
 
 function startInterval() {
 
   setInterval(() => {
     index++;
+    if (index === images.length) index = 0;
+
     updateContent();
 
   }, speed);
