c42aa6f083296cf8ff98e9841a9666ddfc336714
Randy Miller <randy918@gmail.com>
2024-07-07 15:12:52 -0500

    adding footer  7-07-24-1512  Status Quo

--- js/index.js
+++ js/index.js
@@ -1,40 +1,23 @@
-//>  ████████████████████████████████████  GLOBAL VARIABLES
+//? ████████████████████████████████████  DATA
 
-//>  ████████████████████████████████████  QUERY SELECTORS
 
-//_  ████████████████████████████████████  CAROUSEL 1 NO DOTS 
+//_ ████████████████████████████████████  PROMISES
 
-const images = document.querySelectorAll('#carousel img');
-const dots = document.querySelector('#dots');
-const desc = document.querySelector('#desc');
 
-let speed = 7500;
-let index = 0;
-let intervalID;
 
-startInterval();
+//< ████████████████████████████████████  FUNCTIONS
 
-function startInterval() {
 
-    if (intervalID) clearInterval(intervalID);
 
-    intervalID = setInterval(() => {
-        index++;
-        if (index === images.length) index = 0;
-        updateContent();
-    }, speed);
 
-}
 
-updateContent();
+//_$ ████████████████████████████████████  QUERY SELECTORS
 
-function updateContent() {
+//^  ████████████████████████████████████  EVENT LISTENERS
 
-    images.forEach(item => item.classList.remove('active'));
-    images[index].classList.add('active');
+//>  ████████████████████████████████████  REAL PROGRAM
 
-}
+//!  ████████████████████████████████████  FILTER ARRAY 
 
-//>  ████████████████████████████████████  ACTUAL PROGRAM
 
-// startInterval()
+
