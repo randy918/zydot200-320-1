f76fa307aef9618df4bdc3b63f1654afa2d7d798
Randy Miller <randy918@gmail.com>
2024-09-27 14:06:49 -0500

    9-27-24-1406 8 images stacked

--- js/index.js
+++ js/index.js
@@ -2,40 +2,33 @@
 
 //_ ████████████████████████████████████  VARIABLES
 
-let i = 0;
-let images = [];
-let time = 3000;
+
 
 
 
 
 //? ████████████████████████████████████  DATA
 
-images[0] = '1-swiper-1';
-images[1] = '1-swiper-2';
-images[2] = '1-swiper-3';
-images[3] = '1-swiper-4';
-images[4] = '1-swiper-5';
-images[5] = '1-swiper-6';
-images[6] = '1-swiper-7';
-images[7] = '1-swiper-8';
+
+
+//< ████████████████████████████████████  VARIABLES
+
+let imageBackgrounds = document.querySelectorAll(".image__image");
+let imageIndex = 0;
 
 //< ████████████████████████████████████  FUNCTIONS
 
-function changeImg() {
+function changeBackground() {
 
-    document.slide.src = images[i];
-
-    if (i < images.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
-setTimeout('changeImg()', time);
-
+imageBackgrounds[imageIndex].classList.remove("showing");
+imageIndex++;
+if (imageIndex >= imageBackgrounds.length) {
+    imageIndex = 0;
 }
+imageBackgrounds[imageIndex].classList.add("showing");
+console.log(imageIndex);
 
-window.onload = changeImg;
+};
 
 //_$ ████████████████████████████████████  QUERY SELECTORS
 
@@ -45,7 +38,7 @@ window.onload = changeImg;
 
 //>  ████████████████████████████████████  REAL PROGRAM
 
-setInterval(showNextImage, 3000);
+setInterval(changeBackground, 3000);
 
 //!  ████████████████████████████████████  FILTER ARRAY 
 
